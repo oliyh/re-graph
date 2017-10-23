f88da297c8767716641f8a53309fbb88e0e12449
Oliver Hine <git@oliy.co.uk>
2017-10-23 11:35:11 +0100

    Fixing up compilation errors

--- src/re_graph/core.cljs
+++ src/re_graph/core.cljs
@@ -7,44 +7,44 @@
 (re-frame/reg-event-fx
  ::query
  (fn [{:keys [db]} [_ query variables callback-event]]
-   {::do-query [(get-in db [:re-graph :http-url]) query variables callback-event]}))
+   {::send-http [(get-in db [:re-graph :http-url])
+                 {:payload {:query (str "query " query)
+                            :variables variables}}
+                 callback-event]}))
 
 (re-frame/reg-fx
- ::do-query
- (fn [[http-url query variables callback-event]]
-   (go (let [response (a/<! (http/post http-url {:json-params {:query (str "query " query)
-                                                               :variables variables}}))]
-         (re-frame/dispatch (conj response-event (get-in response [:body :data])))))))
+ ::send-http
+ (fn [[http-url {:keys [payload]} callback-event]]
+   (go (let [response (a/<! (http/post http-url {:json-params payload}))]
+         (re-frame/dispatch (conj callback-event (get-in response [:body :data])))))))
 
 (re-frame/reg-event-fx
  ::subscribe
  (fn [{:keys [db]} [_ subscription-id query variables callback-event]]
    {:db (assoc-in db [:re-graph :subscriptions (name subscription-id)] {:callback callback-event})
-    ::do-subscribe [(get-in db [:re-graph :websocket])
-                    subscription-id
-                    query
-                    variables]}))
+    ::send-ws [(get-in db [:re-graph :websocket])
+               {:id subscription-id
+                :type "start"
+                :payload {:query (str "subscription " query)
+                          :variables variables}}]}))
 
 (re-frame/reg-fx
- ::do-subscribe
- (fn [[websocket subscription-id query variables]]
-   (.send websocket (js/JSON.stringify (clj->js {:id subscription-id
-                                                 :type "start"
-                                                 :payload {:query (str "subscription " query)
-                                                           :variables variables}})))))
+ ::send-ws
+ (fn [[websocket payload]]
+   (.send websocket (js/JSON.stringify (clj->js payload)))))
 
 (re-frame/reg-event-fx
- ::on-data
+ ::on-ws-data
  (fn [{:keys [db]} [_ subscription-id payload]]
    (if-let [callback-event (get-in db [:re-graph :subscriptions (name subscription-id) :callback])]
      {:dispatch (conj callback-event (:data payload))}
      (js/console.debug "No callback-event found for subscription" subscription-id))))
 
-(defn- on-message [m]
-  (let [data (js/JSON.parse (.-data e))]
+(defn- on-ws-message [m]
+  (let [data (js/JSON.parse (.-data m))]
     (condp = (.-type data)
       "data"
-      (re-frame/dispatch [::on-data (.-id data) (js->clj (.-payload data) :keywordize-keys true)])
+      (re-frame/dispatch [::on-ws-data (.-id data) (js->clj (.-payload data) :keywordize-keys true)])
 
       (js/console.debug "Ignoring graphql-ws event" (.-type data)))))
 
@@ -56,8 +56,8 @@
  ::init
  (fn [{:keys [db]} [_ {:keys [ws-url http-url]}]]
    (let [ws-url (or ws-url (default-ws-url))
-         ws (js/WebSocket. url "graphql-ws")]
+         ws (js/WebSocket. ws-url "graphql-ws")]
 
-     (aset ws "on-message" on-message)
+     (aset ws "on-message" on-ws-message)
      (assoc-in db [:re-graph] {:websocket ws
                                :http-url (or http-url "/graphql")}))))
