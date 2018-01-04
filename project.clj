2f603475c547a58703e3631b0745d23b2656e4b0
Oliver Hine <git@oliy.co.uk>
2018-01-04 10:48:57 +0000

    Version 0.1.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject re-graph "0.1.3"
+(defproject re-graph "0.1.4-SNAPSHOT"
   :description "GraphQL client for re-frame applications"
   :url "https://github.com/oliyh/re-graph"
   :license {:name "Eclipse Public License"
